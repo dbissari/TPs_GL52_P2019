7b451445a54fbce8b0f8beeb3c2468b1d7c858f8
Deromba Serge Bissari <deromba.bissari@utbm.fr>
2019-04-02 17:26:11 +0200

    Added TP1 package info

--- src/fr/utbm/gl52/tp1/AbstractStack.java
+++ src/fr/utbm/gl52/tp1/AbstractStack.java
@@ -5,7 +5,7 @@ package fr.utbm.gl52.tp1;
 
 /**
  * implements {@link IStack}
- * An abstract stack class created pool common behavior of stacks
+ * An abstract stack class created to pool common behavior of stacks
  * 
  * @author dbissari
  * @param <T> The type of the element which will be stored in the stack
---
+++ src/fr/utbm/gl52/tp1/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * The package containing TP1 sources
+ * 
+ * @author dbissari
+ */
+package fr.utbm.gl52.tp1;
